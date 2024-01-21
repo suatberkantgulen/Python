3e3733d7cbd18d71c5e4100f8775f21f1f1ccb37
suatberkantgulen <suatberkantgulen@gmail.com>
2024-01-21 09:20:34 +0300

    Dictionary Practice

---
+++ Practice/Dictionary/01_DefineDictionary.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# Suat Berkant Gülen
+
+sözlük = {"kitap" : "book",
+		"bilgisayar" : "computer",
+		"programlama": "programming",
+		"dil" : "language",
+		"defter" : "notebook"}
+
+print("Dicstionary", sözlük)
+print("Type : ", type(sözlük))
+print("Length : ", len(sözlük))
---
+++ Practice/Dictionary/02_AccessingDictionaryItems.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# Suat Berkant Gülen
+
+sözlük = {"kitap" : "book",
+		"bilgisayar" : "computer",
+		"programlama": "programming",
+		"dil" : "language",
+		"defter" : "notebook"}
+
+
+for nesne in sözlük:
+	print("Sözlük değişkeni : ", nesne)
+	print("Değişken değeri : ", sözlük[nesne])
+
